06d1325cd074524b056e9f94fd1f49822ccf2320
CCadena <ccadena@air-inc.com>
2017-11-14 13:14:05 -0500

    adding browser sync on lesson3

--- lessons/03-navigating-with-link/index.js
+++ lessons/03-navigating-with-link/index.js
@@ -4,11 +4,10 @@ import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
-
 render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}/>
-    <Route path="/repos" component={Repos}/>
-    <Route path="/about" component={About}/>
-  </Router>
+    <Router history={hashHistory}>
+        <Route path="/" component={App} />
+        <Route path="/repos" component={Repos} />
+        <Route path="/about" component={About} />
+    </Router>
 ), document.getElementById('app'))
